d8bb17ed1511291799a75bb1a0c2796d61e2ad4d
epicodus <student@epicodus.com>
2013-10-09 09:53:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Keypad::Application.config.secret_key_base = 'd4d4795a33e68e8e93c0ca60b33a3cebb2f73bbf55a0a5142606650e232c9f0b2a1391cc46e242d9d3a647d37a42bdec41f9effa94d804b6c0c10da5b54b347f'
